836b11fb6c3c1be325badc44182e522271c53a5e
diate.com <diate.rpps@gmail.com>
2025-07-28 14:50:47 -0300

    server.js

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,15 @@ const rotatividadeRoutes = require('./routes/rotatividade');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: 'https://whatsaprotatividade.netlify.app'
-}));
-app.use(express.json());
+const corsOptions = {
+  origin: 'https://whatsaprotatividade.netlify.app',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+;
 
 
 // Rotas
